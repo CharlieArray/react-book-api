e0d6f6a4cf0dfb6d8aa6d9d6d02b7f2e42ef04fc
Charles Ybarra <59151304+CharlieArray@users.noreply.github.com>
2020-11-26 18:13:49 -0600

    finalized fetch GET results

--- src/BookApp.js
+++ src/BookApp.js
@@ -2,115 +2,132 @@ import React from "react";
 import "./App.css";
 import BookHeader from "./components/BookHeader";
 import BookSearchForm from "./components/BookSearchForm";
-import BookResults from './components/BookResults'
+import BookResults from "./components/BookResults";
 
 class BookApp extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-      this.state = {
-        q: "",
-        filter: 'ebooks',
-        orderBy: 'relevance',
-        key: 'AIzaSyDGTqXk8Hd-ElsKxKXqhtkth7Ue10-LBMU',
-        results: []
-      };
+    this.state = {
+      q: "",
+      filter: "ebooks",
+      orderBy: "relevance",
+      key: "AIzaSyDGTqXk8Hd-ElsKxKXqhtkth7Ue10-LBMU",
+      results: [],
+    };
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-}
-
-//runs after component output has been rendered to DOM
-componentDidMount() {
-
+  //runs after component output has been rendered to DOM
+  searchBooks() {
     const orderBy = this.state.orderBy;
     let filter = this.state.filter;
     let q = this.state.q;
     let key = "AIzaSyDGTqXk8Hd-ElsKxKXqhtkth7Ue10-LBMU";
-    let queryArray = q.replace(/ /g , '+');
+    let queryArray = q.replace(/ /g, "+");
+    console.log(queryArray)
     let stringArray = [queryArray, filter, orderBy, key];
-    let mappedValuesArray = stringArray.map(function(params){
+    let mappedValuesArray = stringArray.map(function (params) {
       return params + "&";
-    })
-    let keyArray = Object.keys(this.state);
-    
-    let finalOuput = [];
-       mappedValuesArray.forEach(function(items, idx){
-       finalOuput.push(`${keyArray[idx]}=${items[idx]}`)
-      })
+    });
+    console.log(mappedValuesArray)
 
-    let stringifiedOutput= finalOuput.join();
-    let finalURLstring = stringifiedOutput.replace(/,/g,"")
+    let keyArray = Object.keys(this.state);
+    console.log(keyArray)
+
+    let finalOuput = [];
+    // mappedValuesArray.forEach(function (item, idx) {
+    //   finalOuput.push(`${keyArray[idx]}=${item}`);
+    // });
+
+    let queryValues = {
+      orderBy: this.state.orderBy,
+      filter: this.state.filter,
+      q: this.state.q.replace(/ /g, "+"),
+      key: "AIzaSyDGTqXk8Hd-ElsKxKXqhtkth7Ue10-LBMU"
+    }
+
+    const queryString = Object.keys(queryValues).map(key => `${key}=${queryValues[key]}`)
+    .join("&")
+
+    for (let i = 0; i < mappedValuesArray.length; i++){
+      finalOuput.push(`${keyArray[i]}=${mappedValuesArray[i]}`)
+    }
+    console.log(queryString)
+
+
+    let stringifiedOutput = finalOuput.join();
+    let finalURLstring = stringifiedOutput.replace(/,/g, "");
+    console.log(finalURLstring)
 
     let baseURL = "https://www.googleapis.com/books/v1/volumes?";
-    let fetchURL = baseURL+finalURLstring;
-  
+    let fetchURL = baseURL + finalURLstring;
 
-  fetch(fetchURL)
-  .then(response => {
-    if(!response.ok) {
-      throw new Error('Something went wrong, please try again later.')
-    }
-    return response;
-  })
-  .then(response => response.json())
-  .then(data => { 
+    fetch(fetchURL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong, please try again later.");
+        }
+        return response;
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        // let dataKeys = Object.entries(data);
+        console.log(data);
 
-  let dataKeys = Object.entries(data)
-  console.log(dataKeys)
+        this.setState({
+          // results: dataKeys[2][1],
+          results: data.items
 
-    this.setState({
-        results: dataKeys[2][1]
-    });
-  })
-  .catch(err => {
-    this.setState({
-      error: err.message
-    });
-  });
-}
-
-
-handleChange(value){
-  this.setState({q: value})
-}
-
-handleSubmit(event){
-  event.preventDefault();
-  if(this.state.q.length > 1){
-  this.componentDidMount();
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          error: err.message,
+        });
+      });
   }
-}
 
-setSelectedBook(selected){
-  this.setState({
-      filter: selected
-  })
-}
+  handleChange(value) {
+    this.setState({ q: value });
+    console.log(value);
+  }
 
-setSelectedSort(selected){
-this.setState({
-      orderBy: selected
-  })
-}
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.q.length > 1) {
+      this.searchBooks();
+    }
+  }
+
+  setSelectedBook(selected) {
+    this.setState({
+      filter: selected,
+    });
+  }
+
+  setSelectedSort(selected) {
+    this.setState({
+      orderBy: selected,
+    });
+  }
 
   render() {
     return (
       <div>
         <div className="BookApp">
-          <BookHeader/>
+          <BookHeader />
           <BookSearchForm
-              handleChange = {this.handleChange}
-              handleSubmit = {this.handleSubmit}
-              changeHandlerBook = {selected => this.setSelectedBook(selected)}
-              changeHandlerSort = {selected => this.setSelectedSort(selected)}
-              filter = {this.state.filter}
-              orderBy = {this.state.orderBy}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            changeHandlerBook={(selected) => this.setSelectedBook(selected)}
+            changeHandlerSort={(selected) => this.setSelectedSort(selected)}
+            filter={this.state.filter}
+            orderBy={this.state.orderBy}
           />
-           <BookResults
-          results = {this.state.results}
-           />
+          <BookResults results={this.state.results} />
         </div>
       </div>
     );
--- src/components/BookResults.js
+++ src/components/BookResults.js
@@ -1,40 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class BookResults extends Component {
+  render() {
 
-    render() {
-        
-        // let results = this.props.results;
-
-        // for (let i = 0; i < results.length; i++){
-        //     if (   ){
-        //     let title = results[0].volumeInfo.title;
-        //     let author = results[0].volumeInfo.authors[0];
-        //     let pubdate = results[0].volumeInfo.publishedDate;
-        //     let description = results[0].volumeInfo.description;
-        //     let pageCount = results[0].volumeInfo.pageCount;
-        //     let imageLinks = results[0].volumeInfo.imageLinks.smallThumbnail;
-        //     let infoLink = results[0].volumeInfo.infoLink;
-        //     }
-        // }
-
-        //{/* <a href="url">link text</a> */}
-
-
-        // <h1>Results: </h1>
-        // <h4> title: {title} </h4>
-        // <h4> author:{author} </h4>
-        // <h4> description:{description} </h4>
-        // <h4> pubdate:{pubdate} </h4>
-        // <h4> pageCount:{pageCount} </h4>
-        // <img src={imageLinks} alt="thumbnail" width="auto" height="auto"/>
-        // <h4> infoLink:{infoLink} </h4>
-
-
-        return (
-             <div>
-
-             </div>
-             )
-    }
+    return (
+      <div>
+        {this.props.results.map((book) => (
+          <div>
+            <h4> title: {book.volumeInfo.title} </h4>
+            <h4> author:{book.volumeInfo.authors[0]} </h4>
+            <h4> description:{book.volumeInfo.description} </h4>
+            <h4> pubdate:{book.volumeInfo.publishedDate} </h4>
+            <h4> pageCount:{book.volumeInfo.pageCount} </h4>
+            <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="thumbnail" width="auto" height="auto" />
+            <h4> infoLink:{book.volumeInfo.infoLink} </h4>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
